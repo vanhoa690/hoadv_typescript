7cfc1016d4ffd3ab348fb9b239075d9058f16751
hoadv <vanhoa690@gmail.com>
2023-11-10 15:57:00 +0700

    comment Login

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,13 @@
 import './app.css';
 
 export function App() {
-  function Login(isLogged: boolean): void {
-    if (isLogged) {
-      console.log('Ban da dang nhap thanh cong!');
-    } else {
-      console.log('Ban chua dang nhap!');
-    }
-  }
+  // function Login(isLogged: boolean): void {
+  //   if (isLogged) {
+  //     console.log('Ban da dang nhap thanh cong!');
+  //   } else {
+  //     console.log('Ban chua dang nhap!');
+  //   }
+  // }
 
   return (
     <div className="h-screen flex items-center justify-center">
