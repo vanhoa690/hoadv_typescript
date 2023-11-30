b79d823e4235ab6a78300d08fba083043fcdf61e
HoadvRP <hoadv@reactplus.jp>
2023-11-30 10:08:50 +0700

    upload image api

--- src/pages/AdminCreateProduct.tsx
+++ src/pages/AdminCreateProduct.tsx
@@ -7,6 +7,22 @@ import { Category } from "../types/Category";
 
 const AdminCreateProduct = () => {
   const navigate = useNavigate();
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    if (!file) return;
+    setFile(file);
+    handleChangedProductImage(file);
+  };
+
+  const handleChangedProductImage = (file: File) => {
+    const fileReader = new FileReader();
+    fileReader.onload = () => setImageUrl(fileReader.result as string);
+    fileReader.readAsDataURL(file);
+  };
+
   const [productAdd, setProductAdd] = useState<ProductFormParams>({
     title: "",
     category: "",
@@ -39,7 +55,14 @@ const AdminCreateProduct = () => {
     event.preventDefault();
     // validate show error
     try {
-      await axios.post("/products", productAdd);
+      if (file) {
+        const formData = new FormData();
+        formData.append("image", file);
+        const { data } = await axios.post("/upload/single/clound", formData);
+        await axios.post("/products", { ...productAdd, image: data.imageUrl });
+      } else {
+        await axios.post("/products", productAdd);
+      }
       toast.success("Add Product Successfull!");
       navigate("/admin/products");
     } catch (error) {
@@ -89,6 +112,59 @@ const AdminCreateProduct = () => {
                 placeholder="Type product Image"
               />
             </div>
+            <div className="flex items-center justify-center w-full">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    SVG, PNG, JPG or GIF (MAX. 800x400px)
+                  </p>
+                </div>
+                <input
+                  onChange={handleFileChange}
+                  id="dropzone-file"
+                  type="file"
+                  className="hidden"
+                />
+              </label>
+            </div>
+            {imageUrl && (
+              <div className="text-center">
+                <img
+                  className="h-auto max-w-full rounded-lg"
+                  src={imageUrl}
+                  alt="image description"
+                />
+                <button
+                  onClick={() => setImageUrl("")}
+                  className="mt-2 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                >
+                  Remove Image
+                </button>
+              </div>
+            )}
+
             <div className="sm:col-span-2">
               <label
                 htmlFor="category"
